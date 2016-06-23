d241c2c1e38360a72a6c6272ee124df2bffa193d
Felipe Gentil <cdigentil@gmail.com>
2016-06-23 10:47:29 -0300

    Include simplecov and setup spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,28 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'terminal_crontab'
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
